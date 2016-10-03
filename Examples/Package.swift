fb54eea198125530e49bdf831a8f45bf314553f9
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-03 11:02:01 +0300

    Moved examples into swift-zmq-examples repo

--- Examples/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "ZMQExamples",
-    dependencies: [
-        .Package(url: "https://github.com/azawawi/swift-zmq.git", majorVersion: 0, minor: 1),
-    ]
-)
