753849ac46369ddc522ac360e7f6f48d90b8918f
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-03 10:58:06 +0300

    Select example via the command line

--- Examples/main.swift
+++ Examples/main.swift
@@ -1,7 +1,25 @@
 
+import Foundation
 import ZMQ
 
-print(ZMQ.version)
-print(ZMQ.has(.ipc))
+func displayUsage() {
+    let processName = CommandLine.arguments[0]
+    print("\nUsage:")
+    print("\t\(processName) example-name\n")
+    print("example-name can be hwclient or hwserver\n")
+}
 
-try hwclient()
+guard CommandLine.arguments.count >= 1 else {
+    displayUsage()
+    exit(0)
+}
+
+let command = CommandLine.arguments[1]
+switch command {
+    case "hwclient":
+        try hwclient()
+    case "hwserver":
+        try hwserver()
+    default:
+        displayUsage()
+}
